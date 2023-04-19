7565b21c51d5586adfd10801b4ccc569db957415
Kristian Dotson <KDotson387@gmail.com>
2023-04-19 10:42:19 -0500

    question 6

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -78,6 +78,7 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
+  return arr.map((Element) => Math.pow(2, Element));
 };
 
 /* ------------------------------------------------------------------------------------------------
